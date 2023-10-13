ff37bf997c1da3af71860e45c47574ecf7444ead
hemispherium <nkmr357@gmail.com>
2023-10-13 13:25:01 +0900

    feat: Enable closing dialog with clicking back-drop

--- pages/index.js
+++ pages/index.js
@@ -17,15 +17,23 @@ export default function Home({ allPostsData }) {
     dialogRef.current.close();
   }
 
+  function closeDialogByClickingBackDrop(event) {
+    if(event.target === dialogRef.current) {
+      dialogRef.current.close();
+    }
+  }
+
   return (
     <Layout home>
       <Head>
         <title>{siteTitle}</title>
       </Head>
       <button id="open" type="button" onClick={showDialog}>Open</button>
-      <dialog className={utilStyles.dialogTag} ref={dialogRef}>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-        <button id="close" type="button" onClick={closeDialog}>Close</button>
+      <dialog className={utilStyles.dialogTag} ref={dialogRef} onClick={(event) => closeDialogByClickingBackDrop(event)}>
+        <div class="inner">
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+          <button id="close" type="button" onClick={closeDialog}>Close</button>
+        </div>
       </dialog>
       <section className={utilStyles.headingMd}>
         <p>[Your Self Introduction]</p>
