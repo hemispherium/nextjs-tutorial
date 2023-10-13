f87a747472c734b54622e28337e7aa72496f81d7
hemispherium <nkmr357@gmail.com>
2023-10-13 11:23:10 +0900

    feat: Implement an useless modal on top page.

--- pages/index.js
+++ pages/index.js
@@ -4,13 +4,29 @@ import utilStyles from '../styles/utils.module.css';
 import { getSortedPostsData } from '../lib/posts';
 import Link from 'next/link';
 import Date from '../components/date';
+import { useRef } from 'react';
 
 export default function Home({ allPostsData }) {
+  const dialogRef = useRef(null);
+
+  function showDialog() {
+    dialogRef.current.showModal();
+  }
+
+  function closeDialog() {
+    dialogRef.current.close();
+  }
+
   return (
     <Layout home>
       <Head>
         <title>{siteTitle}</title>
       </Head>
+      <button id="open" type="button" onClick={showDialog}>Open</button>
+      <dialog className={utilStyles.dialogTag} ref={dialogRef}>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <button id="close" type="button" onClick={closeDialog}>Close</button>
+      </dialog>
       <section className={utilStyles.headingMd}>
         <p>[Your Self Introduction]</p>
         <p>
